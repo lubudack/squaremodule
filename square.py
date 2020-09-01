d5acd2867f12e7b6d28d5c92267116d4db017db0
lubudack <49934082+lubudack@users.noreply.github.com>
2020-09-01 14:18:06 +0900

    Update square.py

--- square.py
+++ square.py
@@ -17,4 +17,9 @@ def age(n):
     result = int(time.strftime('%Y', time.localtime(time.time()))) - int(n) + 1
     realresult = str(result) + "살입니다."
     return realresult #출력
+h = input("세로값:")
+n = input("가로값:")
+print(square(int(h), int(n)))
+n = input("생년월일:")
+print(age(int(n)))
     
