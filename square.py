835874dff70bd46f9642b897524e61df32388133
lubudack <49934082+lubudack@users.noreply.github.com>
2020-09-01 12:34:57 +0900

    Update square.py

--- square.py
+++ square.py
@@ -7,12 +7,14 @@ def square(h, n):
         raise IndexError
     slist = [int(h), int(n)]
     result = slist[0] * slist[1]
-    return result
+    realresult = str(result) + "가 사각형의 넓이입니다."
+    return realresult
 def age(n):
     try:
         int(n)
     except:
         raise IndexError
     result = int(time.strftime('%Y', time.localtime(time.time()))) - int(n) + 1
-    return result
+    realresult = str(result) + "살입니다."
+    return realresult
     
